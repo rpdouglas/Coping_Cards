1f48e43f78d31b5f8de801308e5aea0c22d85d77
rpdouglas <rpdouglas@gmail.com>
2025-10-15 19:21:50 -0400

    Update cache version from v3 to v4

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', function(e) {
   e.waitUntil(
-    caches.open('coping-cards-v3').then(function(cache) {
+    caches.open('coping-cards-v4').then(function(cache) {
       return cache.addAll([
         './',
         './index.html'
