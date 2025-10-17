3b3252c149ddaf54c6cb83535a4f5ad773643b3c
rpdouglas <rpdouglas@gmail.com>
2025-10-16 20:38:24 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', function(e) {
   e.waitUntil(
-    caches.open('coping-cards-v37').then(function(cache) {
+    caches.open('coping-cards-v38').then(function(cache) {
       return cache.addAll([
         './',
         './index.html',
